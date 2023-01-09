c73ebad8543b93278f5bde0474b16f327eaf609a
daniamustafa <danya.mustafa22@gmail.com>
2023-01-09 13:37:10 +0300

    update

---
+++ src/Class.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Class {
+
+	public static void main(String[] args) {
+		WebDriver driver;
+
+		WebDriverManager.chromedriver().setup();
+
+		driver = new ChromeDriver();
+
+		driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
+	}
+
+}
